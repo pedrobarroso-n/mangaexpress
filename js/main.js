89324e1d422b37798c7fcc407af54eaa27a4633f
pedrobarroso-n <pbn62413@gmail.com>
2024-06-15 19:19:40 -0300

    updates

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 import conectaApi from './conectaApi.js'
 const listaDeMangas = document.querySelector('[data-lista]');
 
-listaDeMangas.innerHTML = ``; //inicializar vazio
-
 async function constroiCard () {
     const conexao = await conectaApi();
+    listaDeMangas.innerHTML = ``;  //iniciar vazio
     conexao.forEach(element => listaDeMangas.innerHTML += `
         <div class="conteudo__lista__item">
         <img src="${element.capa}" alt="capa do manga ${element.titulo} volume ${element.volume}">
         <div class="item__descricao">
         <p>${element.titulo}</p>
-        <p>${element.volume}</p>
+        <p>vol.${element.volume}</p>
         </div>
         <div class="item__descricao">
         <p>R$ ${element.preco}</p>
         <img src="../image/delete.png" alt="icone de remover manga da lista">
         </div>
         </div>
-        `
-    );
+        `)
 
     return listaDeMangas;
 }
